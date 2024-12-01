bed2652054e4ac61153a3e50dcb19a24e0850c03
ELENI ZAFEIRI <sdi2400046@linux10.di.uoa.gr>
2024-12-01 19:26:50 +0200

    c1

--- chal1.c
+++ chal1.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(){
-    int temp,i,j;
+    int temp,i,j,result;
     int list2[1000];
     int list1[1000];
     for(i = 0;i < 1000;i++){
-        scanf("%d %d", &list1[i], &list2[i]);
+        result = scanf("%d %d", &list1[i], &list2[i]);
+        if(result != 2){
+            return 1;
+        }
     }
-
     for(i = 1;i < 1000;i++){
-        for(j = 1000;i > i ;i--){
+        for(j = 999;j > 0 ;j--){
             if(list1[j] < list1[j-1]){
                 temp = list1[j];
                 list1[j] = list1[j-1];
@@ -18,7 +21,7 @@ int main(){
         }
     }
     for(i = 1;i < 1000;i++){
-        for(j = 1000;i > i ;i--){
+        for(j = 999;j > 0 ;j--){
             if(list2[j] < list2[j-1]){
                 temp = list2[j];
                 list2[j] = list2[j-1];
@@ -27,10 +30,8 @@ int main(){
         }
     }
     long long int s = 0;
-    int d[1000];
     for(i = 0;i < 1000;i++){
-        d[i] = list1[i] + list2[i];
-        s = s + d[i];
+        s = s + labs(list1[i] - list2[i]);
     }
     printf("%lld\n", s);
     return 0;
